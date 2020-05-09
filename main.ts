5fb9943acf5babf02c3cc367657c3139701ef191
Adam Grabara <44620474+agrabara@users.noreply.github.com>
2020-05-09 12:55:13 +0200

    Pozbywamy się głowy, dodajemy ciało węża, generowanie owoców tak aby nie pojawiały się pod ciałem węża

--- main.ts
+++ main.ts
@@ -28,38 +28,65 @@ input.onButtonPressed(Button.B, function () {
         DirY = 0
     }
 })
-let DirY = 0
-let DirX = 0
+let newy = 0
+let newx = 0
+let jeszczeraz = 0
 let wynik = 0
-let Głowa = game.createSprite(2, 0)
-DirX = 1
+let GlowaY = 0
+let GlowaX = 0
+let Cialo: game.LedSprite[] = []
+let DirX = 0
+let DirY = 0
 DirY = 0
-let Owoc = game.createSprite(Math.randomRange(0, 4), Math.randomRange(0, 4))
+DirX = 1
+let Owoc = game.createSprite(4, 4)
 Owoc.set(LedSpriteProperty.Brightness, 100)
+Cialo.insertAt(0, game.createSprite(GlowaX, GlowaY))
 basic.forever(function () {
-    if (Głowa.get(LedSpriteProperty.X) == 4 && DirX == 1) {
-        game.gameOver()
+    if (GlowaX == 4 && DirX == 1) {
         game.setScore(wynik)
-    } else if (Głowa.get(LedSpriteProperty.X) == 0 && DirX == -1) {
         game.gameOver()
+    } else if (GlowaX == 0 && DirX == -1) {
         game.setScore(wynik)
-    } else if (Głowa.get(LedSpriteProperty.Y) == 0 && DirY == -1) {
         game.gameOver()
+    } else if (GlowaY == 0 && DirY == -1) {
         game.setScore(wynik)
-    } else if (Głowa.get(LedSpriteProperty.Y) == 4 && DirY == 1) {
         game.gameOver()
+    } else if (GlowaY == 4 && DirY == 1) {
         game.setScore(wynik)
+        game.gameOver()
     }
-    Głowa.change(LedSpriteProperty.X, DirX)
-    Głowa.change(LedSpriteProperty.Y, DirY)
-    if (Głowa.get(LedSpriteProperty.X) == Owoc.get(LedSpriteProperty.X) && Głowa.get(LedSpriteProperty.Y) == Owoc.get(LedSpriteProperty.Y)) {
-        Owoc.set(LedSpriteProperty.X, Math.randomRange(0, 4))
-        Owoc.set(LedSpriteProperty.Y, Math.randomRange(0, 4))
+    GlowaX += DirX
+    GlowaY += DirY
+    for (let value of Cialo) {
+        if (GlowaX == value.get(LedSpriteProperty.X) && GlowaY == value.get(LedSpriteProperty.Y)) {
+            game.setScore(wynik)
+            game.gameOver()
+        }
+    }
+    Cialo.insertAt(0, game.createSprite(GlowaX, GlowaY))
+    if (GlowaX == Owoc.get(LedSpriteProperty.X) && GlowaY == Owoc.get(LedSpriteProperty.Y)) {
+        jeszczeraz = 1
+        while (jeszczeraz == 1) {
+            jeszczeraz = 0
+            newx = Math.randomRange(0, 4)
+            newy = Math.randomRange(0, 4)
+            for (let value2 of Cialo) {
+                if (newx == value2.get(LedSpriteProperty.X) && newy == value2.get(LedSpriteProperty.Y)) {
+                    jeszczeraz = 1
+                }
+            }
+        }
+        Owoc.set(LedSpriteProperty.X, newx)
+        Owoc.set(LedSpriteProperty.Y, newy)
         wynik += 1
         Owoc.set(LedSpriteProperty.Brightness, 100)
+        Owoc.set(LedSpriteProperty.Blink, 0)
         if (Math.randomRange(0, 3) == 0) {
             Owoc.set(LedSpriteProperty.Blink, 100)
         }
+    } else {
+        Cialo.pop().delete()
     }
     basic.pause(750)
 })
